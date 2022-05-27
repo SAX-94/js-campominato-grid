3c7d4a4de19d92e81533af7304be5153e9d9c5bf
SAX_94 <matteosacchi94@gmail.com>
2022-05-27 22:06:41 +0200

    created script necessry to generate grid

---
+++ js/griglia.js
@@ -0,0 +1,31 @@
+const gridContainer = document.querySelector(".grid-container");
+// Creo una griglia con celle 50x50
+// Uso un ciclo per creare le celle
+// Creo un div per ogni cella
+// Appendo la cell a grid-container
+
+/**numero di celle in orizzontale
+ * @param {number} xCells - numero di celle in orizzontale
+ * @param {number} yCells - numero di celle in verticale
+ */
+function createGrid(xCells, yCells) {
+    const cellsNumber = xCells * yCells;
+
+    console.log(cellsNumber);
+    // Definisco la larghez<a del grid-container in base al numero di celle orizzontali 
+    // @ts-ignore
+    gridContainer.style.width = `calc(var(--cell-size) * ${xCells})`;
+
+    for (let i = 0; i < cellsNumber; i++) {
+        // Creo un numero che incrementa di 1 a ogni ciclo
+        const cellNumber = i + 1 ;
+        // Creo un div per ogni cella.
+        const cell = document.createElement("div");
+        // Finché non assegno la classe "cell" ai div, essi rimangono contenitori vuoti e invisibili.
+        cell.classList.add("cell");
+        // Inserisco il numero incrementale nella cella
+        cell.append(cellNumber.toString());
+        // Inserisco il div nel grid-container.
+        gridContainer.append(cell);
+    }
+}
